be6ef00dd1674063e8df558a9ba3fc1788b7dd37
Swethavarnaa <swethavarnaa@gmail.com>
2021-06-14 13:20:44 +0530

    added methods for customer service.

--- Backend/src/main/java/com/varnaa/bookdate/service/CustomerService.java
+++ Backend/src/main/java/com/varnaa/bookdate/service/CustomerService.java
@@ -1,13 +1,17 @@
-package com.varnaa.bookdate.repository;
+package com.varnaa.bookdate.service;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.varnaa.bookdate.config.RestTemplateService;
 import com.varnaa.bookdate.deserializer.CustomDeserializer;
 import com.varnaa.bookdate.model.Customer;
+import com.varnaa.bookdate.model.Subscription;
+import com.varnaa.bookdate.repository.CustomerRepository;
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 public class CustomerService {
@@ -27,18 +31,36 @@ public class CustomerService {
         return customerRepository.getOne(customerId);
     }
 
-    public boolean save(Customer customer) throws JsonProcessingException {
+    public Customer save(Customer customer, String URI) throws JsonProcessingException {
         try {
-            ResponseEntity<String> responseEntity = restTemplateService.postCustomer(customer);
+            ResponseEntity<String> responseEntity = restTemplateService.postCustomer(customer, URI);
             logger.trace("posted new customer to Zoho Subscription");
             customer.setCustomerId(customDeserializer.getCustomerId(responseEntity));
             customerRepository.save(customer);
             logger.trace("customer successfully saved to local db");
-            return true;
+            return customer;
         } catch (Exception e) {
             e.printStackTrace();
 
         }
+        return null;
+    }
+
+    public boolean deleteById(String customerId) {
+        try {
+            customerRepository.deleteById(customerId);
+            logger.trace("deleted customer -> " + customerId);
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         return false;
     }
+
+    public List<Subscription> getAllSubscriptionsById(String customerId) {
+        logger.trace("retrieved subscriptions for customer ->" + customerId);
+        return customerRepository.findAllSubscriptionsByCustomerId(customerId);
+    }
+
+
 }
