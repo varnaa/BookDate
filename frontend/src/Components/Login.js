589eddc42863494b6b3f8332a2019dee58735fa6
Swethavarnaa <swethavarnaa@gmail.com>
2021-06-16 12:42:19 +0530

    added form logic

--- frontend/src/Components/Login.js
+++ frontend/src/Components/Login.js
@@ -1,6 +1,32 @@
 import React, { Component } from 'react'
+import CustomerService from '../services/CustomerService';
 
 export default class Login extends Component {
+    constructor(props){
+        super(props);
+        this.state={
+            "email":"",
+            "password":""
+        }
+
+        handleChange = (event) => {
+            this.setState({ [event.target.name]: event.target.value });
+          };
+
+
+        handleSubmit = (event) => {
+            event.preventDefault();
+            // makes call to backend api
+            CustomerService.login(this.state).then((response) => {
+              if (response.status >= 200 && response.status < 300) {
+                // gets customer id from backend and stores it in session
+                localStorage.setItem("customerId",response.data);
+              }else{
+                  alert("Invalid user!");
+              }
+            });
+        };
+    }
     render() {
         return (
             <div>
--- frontend/src/Components/Signup.js
+++ frontend/src/Components/Signup.js
@@ -1,6 +1,52 @@
 import React, { Component } from 'react'
+import CustomerService from '../services/CustomerService';
 
 export default class Signup extends Component {
+    constructor(props){
+        super(props);
+        this.state={
+            "display_name": "",
+            "salutation": "",
+            "first_name": "",
+            "last_name": "",
+            "email": "",
+            "mobile": 0,
+            "billing_address": {
+                "street": "",
+                "city": "",
+                "state": "",
+                "zip": 0,
+                "country": ""
+            },
+            "shipping_address": {
+                "street": "",
+                "city": "",
+                "state": "",
+                "zip": 0,
+                "country": ""
+            },
+            "user":{
+                "email":"",
+                "password":""
+            },
+            "subscriptions":[]
+        }
+
+        handleChange = (event) => {
+            this.setState({ [event.target.name]: event.target.value });
+          };
+
+        handleSubmit = (event) => {
+            event.preventDefault();
+            CustomerService.postCustomer(this.state).then((response) => {
+              if (response.status >= 200 && response.status < 300) {
+                 // todo redirect to login component
+              }else{
+                  alert("Unable to sign up!");
+              }
+            });
+        };
+    }
     render() {
         return (
             <div>
