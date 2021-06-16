db5d638a766b0fc3b9cd8eedf410c7d36b7f4638
Swethavarnaa <swethavarnaa@gmail.com>
2021-06-16 12:07:26 +0530

    Added routing methods

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,22 +3,15 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <Router>
+     <switch>
+       <Route exact path="/" component={Home}/>
+       <Route exact path="/login" component ={Login}/>
+       <Route exact path="/signup" component = {Signup} />
+       <Route exact path="/checkout" component={Checkout} />
+       <Route exact path="/profile" component={Profile} />
+     </switch>
+   </Router>
   );
 }
 
